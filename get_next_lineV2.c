4b844d279ffb071d3cce220c3a599dd01847fa5d
Pierre Nardozi <pierrenardozi@MacBook-Pro-de-Pierre.local>
2017-12-05 00:35:13 +0100

    voir probl;e 2 ou 3 fichier

--- get_next_lineV2.c
+++ get_next_lineV2.c
@@ -26,41 +26,84 @@ static int			ft_eol(char const *s, int c)
 	return (0);
 }
 
-static int			ft_read(const int fd, char *save)
+static int			ft_read(const int fd, char **save)
 {
-	char *buff;
-	size_t end;
+	char	*tmp;
+	char	*buff;
+	size_t	end;
+	int	i;
+	int	j;
+	int	k;
 
-	if (!(buff = ft_strnew(BUFF_SIZE)))
-		return (-1);
-	if ((end = read(fd, buff, BUFF_SIZE)) == -1)
-		return (-1);
-	ft_strcpy(save, buff);
-	ft_strdel(&buff);
-	if (end == 0)
-		return (0);
+	i = 0;
+	j = 0;
+	k = 1;
+
+
+	while (j <= 0)
+	{
+		if (!(buff = ft_strnew(BUFF_SIZE)))
+			return (-1);
+		if (!j && *save)
+		{
+			tmp = ft_strdup(*save);
+			ft_strdel(save);
+			*save = ft_strnew(BUFF_SIZE * k);
+			ft_strcpy(*save, tmp);
+			ft_strdel(&tmp);
+		}
+		else
+			*save = ft_strnew(BUFF_SIZE);
+		if ((int)(end = read(fd, buff, BUFF_SIZE)) == -1)
+			return (-1);
+		ft_strcat(*save, buff);
+		ft_strdel(&buff);
+		while (save[0][i] != '\0')
+		{
+			if (save[0][i] == '\n' || save[0][i] == (char)EOF)
+				j++;
+			i++;
+		}
+		if (end == 0)
+			return (0);
+		k++;
+
+	}
 	return (1);
+}
+
+int	ft_isin(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n' || str[i] == EOF)
+			return (1);
+		i++;
+	}
+	return 0;
 }
 
 static int			ft_cpy(char **line, int fd, char **str)
 {
 	char	*tmp;
 	int	i;
-	
+
 	i = 0;
 	if (!*str)
-	{
-		if (!(*str = ft_strnew(BUFF_SIZE)))
-			return (-1);
-		ft_read(fd, *str);
-	}
+		ft_read(fd, str);
 	if (str[0] != '\0')
 	{
+		if (!(ft_isin(*str)))
+			ft_read(fd, str);
 		i = ft_eol(*str, '\n');
 		if (!(*line = ft_strsub(*str, 0, i)))
 			return (-1);
+		dprintf(1, "str = %s|\n", *str);
 		i++;
-		if (i > ft_strlen(*str))
+		if (i > (int)ft_strlen(*str))
 		{
 			free(str);
 			*str = ft_strnew(1);
@@ -77,8 +120,30 @@ static int			ft_cpy(char **line, int fd, char **str)
 
 int				get_next_line(const int fd, char **line)
 {
-	static char	*save;
+	static t_gnl_list	*list;
+	t_gnl_list		*cursor;
 
-	return (ft_cpy(line, fd, &save));
+	cursor = list;
+	if (!list)
+	{
+		if (!(list = ft_memalloc(sizeof(t_gnl_list))))
+			return (-1);
+		list->fd = fd;
+		return (ft_cpy(line, fd, &list->data));
+	}
+	if (cursor)
+	{
+		while (cursor->next != NULL && cursor->fd != fd)
+			cursor = cursor->next;
+		if (cursor->fd == fd)
+			return(ft_cpy(line, fd, &cursor->data));
+		else
+		{
+			if (!(cursor = ft_memalloc(sizeof(t_gnl_list))))
+				return (-1);
+			cursor->fd = fd;
+			return (ft_cpy(line, fd, &cursor->data));
+		}
+	}
 	return (0);
 }
--- main.c
+++ main.c
@@ -15,52 +15,73 @@
 int		main(int argc, char **argv)
 {
 	char    *line;
-	int     fd; 
+//	int     fd; 
 	int		i;
 	if (argc)
 		i = 0;
 	i = 0;
-	fd = open(argv[1], O_RDONLY);
-//		while ((i = get_next_line(fd, &line)))
-//			dprintf(1, "retour fonction = %d phrase : %s\n", i, line);
+//		fd = open(argv[1], O_RDONLY);
+//			while ((i = get_next_line(fd, &line)))
+//				dprintf(1, "retour fonction = %d phrase : %s\n", i, line);
+//		i = get_next_line(1, &line);
+//		printf("line : %s\n", line);
+/*		ft_strdel(&line);
+		i = get_next_line(fd, &line);
+		printf("line : %s\n", line);
+		ft_strdel(&line);
 	i = get_next_line(fd, &line);
-	printf("line : %s\n", line);
-	ft_strdel(&line);
-	/*	dprintf(1, "retour fonction = %d\n", i);
-	iint	fd1 = open(argv[2], O_RDONLY);
-	int	fd2 = open(argv[3], O_RDONLY);
-	int	fd3 = open(argv[4], O_RDONLY);
-	int fd4 = open(argv[5], O_RDONLY);
-	//	ft_putendl("1ER APPEL FICHIER 1");
-	get_next_line(0, &line);
-	//	ft_putendl("1ER APPEL FICHIER 2");
-	get_next_line(fd2, &line);
-	//	ft_putendl("2EME APPEL FICHIER 1");
-	get_next_line(fd2, &line);
-	//	ft_putendl("1ER APPEL FICHIER 3");
-	get_next_line(fd1, &line);
-	//	ft_putendl("2EME APPEL FICHIER 2");
-	get_next_line(fd1, &line);
-	//	ft_putendl("3EME APPEL FICHIER 1");
-	get_next_line(fd2, &line);
-	//	ft_putendl("2EME APPEL FICHIER 3");
-	get_next_line(fd, &line);
-	//	ft_putendl("3EME APPEL FICHIER 2");
-	get_next_line(fd3, &line);
-	get_next_line(fd2, &line);
-	get_next_line(fd, &line);
-	get_next_line(fd4, &line);
-	get_next_line(fd4, &line);
-	get_next_line(fd4, &line);
-	get_next_line(fd4, &line);
-	get_next_line(fd1, &line);
-	get_next_line(fd3, &line);
-	get_next_line(fd3, &line);
-	get_next_line(fd2, &line);
-	get_next_line(fd, &line);
-	get_next_line(fd1, &line);
-	get_next_line(fd2, &line);
-	get_next_line(fd, &line);
+		printf("line : %s\n", line);
+		ft_strdel(&line);
+		i = get_next_line(fd, &line);
+		printf("line : %s\n", line);
+		ft_strdel(&line);
+
 */
+
+	int	fd1 = open(argv[1], O_RDONLY);
+	int	fd2 = open(argv[2], O_RDONLY);
+	int	fd3 = open(argv[3], O_RDONLY);
+	ft_putendl("1ER APPEL FICHIER 1");
+	get_next_line(fd1, &line);
+	
+	dprintf(1,"%s\n", line);
+	ft_putendl("1ER APPEL FICHIER 2");
+	get_next_line(fd2, &line);
+	dprintf(1,"%s\n", line);
+//	ft_strdel(&line);
+	ft_putendl("2EME APPEL FICHIER 1");
+	get_next_line(fd1, &line);
+	dprintf(1,"%s\n", line);
+	ft_strdel(&line);
+//	ft_putendl("1ER APPEL FICHIER 3");
+//	get_next_line(fd3, &line);
+//	dprintf(1,"%s\n", line);
+	ft_putendl("2EME APPEL FICHIER 2");
+	get_next_line(fd2, &line);
+	dprintf(1,"%s\n", line);
+	ft_putendl("3EME APPEL FICHIER 1");
+	get_next_line(fd1, &line);
+	dprintf(1,"%s\n", line);
+//	ft_putendl("2EME APPEL FICHIER 3");
+//	get_next_line(fd3, &line);
+//	dprintf(1,"%s\n", line);
+	ft_putendl("3EME APPEL FICHIER 2");
+	get_next_line(fd2, &line);
+	dprintf(1,"%s\n", line);
+/*		get_next_line(fd2, &line);
+		get_next_line(fd, &line);
+		get_next_line(fd4, &line);
+		get_next_line(fd4, &line);
+		get_next_line(fd4, &line);
+		get_next_line(fd4, &line);
+		get_next_line(fd1, &line);
+		get_next_line(fd3, &line);
+		get_next_line(fd3, &line);
+		get_next_line(fd2, &line);
+		get_next_line(fd, &line);
+		get_next_line(fd1, &line);
+		get_next_line(fd2, &line);
+		get_next_line(fd, &line);
+		*/
 	return (0);
 }
